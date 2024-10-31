509cc1336e14bc04743fa9d4e183578ace70f657
SerkovDanil <danil.serkov24@gmail.com>
2024-10-31 12:34:21 +0300

    Update lab_2_2.py

--- Laboratory 2/lab_2_2.py
+++ Laboratory 2/lab_2_2.py
@@ -8,12 +8,11 @@ increase = 0.03  # Ежемесячный рост цен
 m = 0  # месяцев
 money_capital = 0
 while m < months:
-    c = salary - spend * (1 + m * increase)
-    money_capital = money_capital + c * (-1)
-
+    c = salary - spend
+    money_capital = money_capital + c
+    spend = spend * (1 + increase)
     m = m+1
 
-
-
+money_capital = int(money_capital * (-1))
 
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital )
